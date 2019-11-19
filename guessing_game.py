6912b1a880ca7d9aa9ee9b4ce227d59199e11455
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-19 08:20:44 +0200

    Rename variables and arguments more clearly

--- guessing_game.py
+++ guessing_game.py
@@ -16,26 +16,26 @@ def populate_list(limit):
     return lst
 
 
-def guess(list_letter, limit):
+def guess(working_list, limit):
     for i in range(10):
-        g = int(input(f"Enter an integer from 1 to {limit}: "))
-        while list_letter[i] != g:
-            if g < list_letter[i]:
+        number_guess = int(input(f"Enter an integer from 1 to {limit}: "))
+        while working_list[i] != number_guess:
+            if number_guess < working_list[i]:
                 print("guess is low")
-                g = int(input(f"Enter an integer from 1 to {limit}: "))
-            elif g > list_letter[i]:
+                number_guess = int(input(f"Enter an integer from 1 to {limit}: "))
+            elif number_guess > working_list[i]:
                 print("guess is high")
-                g = int(input(f"Enter an integer from 1 to {limit}: "))
+                number_guess = int(input(f"Enter an integer from 1 to {limit}: "))
             else:
                 break
         print("you guessed it!")
 
 
 def main():
-    a = populate_list(99)
-    b = populate_list(49)
-    guess(a, 99)
-    guess(b, 49)
+    list_a = populate_list(99)
+    list_b = populate_list(49)
+    guess(list_a, 99)
+    guess(list_b, 49)
 
 
 main()
